960099c4178021a72fe51ce173c226706ea00cef
Sankalp <sankalpchauhan.me@gmail.com>
2023-12-10 22:32:59 +0530

    initial commit

--- app/src/main/java/me/sankalpchauhan/sdksample/MainActivity.kt
+++ app/src/main/java/me/sankalpchauhan/sdksample/MainActivity.kt
@@ -2,10 +2,12 @@ package me.sankalpchauhan.sdksample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.sankalpchauhan.droidcraft.sdk.network.RetrofitFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        RetrofitFactory()
     }
 }
